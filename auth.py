30116f67e66d8b236d71a457d7b2516a0e84ca50
Narwhalish <bryao@ctemc.org>
2018-05-20 00:51:00 -0400

    Complete parse

--- auth.py
+++ auth.py
@@ -1,14 +1,55 @@
-import tweepy
+from twython import TwythonStreamer
 import json
-from pprint import pprint #pretty print to ease debugging
+import string
 
-# Consumer keys and access tokens, used for OAuth
+def make_printable(s):
+    printable = set(string.printable)
+    return ''.join(filter(lambda x: x in printable, s))
+
+def process_tweet(tweet):
+    d = {tweet['user']['id']:{
+    # 'name': tweet['user']['name'],
+    # 'screen_name': tweet['user']['name'],
+    # 'created_at': tweet['created_at'],
+    # 'text': tweet['text']
+    'name': make_printable(tweet['user']['name']),
+    'screen_name': make_printable(tweet['user']['name']),
+    'created_at': make_printable(tweet['created_at']),
+    'text': make_printable(tweet['text'])
+    }}
+    return d
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if data['lang'] == 'en':
+            tweet_data = process_tweet(data)
+            self.save_to_json(tweet_data)
+
+    def on_error(self, status_code, data):
+        print(status_code, data)
+        self.disconnect()
+
+    def save_to_json(self, tweet):
+        with open('search_results.json') as f:
+            data = json.load(f)
+
+        data.update(tweet)
+
+        with open('search_results.json', mode = 'a') as f:
+            json.dump(data, f)
+
+
+with open('search_results.json', mode = 'w') as f:
+    primer = {}
+    json.dump(primer, f)
+
 with open('twitter_credentials.json') as f:
     credentials = json.load(f)
 
-# OAuth process, using the keys and tokens
-auth = tweepy.OAuthHandler(credentials['CONSUMER']['KEY'], credentials['CONSUMER']['SECRET'])
-auth.set_access_token(credentials['ACCESS']['TOKEN'], credentials['ACCESS']['SECRET'])
+APP_KEY = credentials['CONSUMER_KEY']
+APP_SECRET = credentials['CONSUMER_SECRET']
+ACCESS_TOKEN = credentials['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = credentials['ACCESS_TOKEN_SECRET']
 
-# Creation of the actual interface, using authentication
-api = tweepy.API(auth)
+stream = MyStreamer(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+stream.statuses.filter(track = 'death,rainbow,kill')
